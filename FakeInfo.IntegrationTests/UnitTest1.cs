769bad3e2424052d8c165d2be31ce006e096c9dd
Alh004 <alh004@edu.zealand.dk>
2026-03-31 20:10:25 +0200

    m

---
+++ FakeInfo.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace FakeInfo.IntegrationTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
